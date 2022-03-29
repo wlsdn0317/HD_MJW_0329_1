482d0f7d623dbd83acf80fddb9437766f991080d
wlsdn0317 <78064428+wlsdn0317@users.noreply.github.com>
2022-03-29 21:21:24 +0900

    no message

--- Test_2/Program.cs
+++ Test_2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        int i = 2;
+        int j = 1;
+        while (i < 7)
+        {
+            while (j < 10)
+            {
+                Console.WriteLine($"{i}*{j} = {i * j}");
+                j++;
+            }
+            Console.WriteLine("\n");
+            j = 1;
+            i += 2;
+        }
     }
 }
---
+++ Test_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int i = 1;
+        while (i < 21)
+        {
+            if(i %2 != 0)
+            {
+                sum += i;
+            }
+            i++;
+        }
+        Console.WriteLine($"홀수의 합은 = {sum}");
+    }
+}
